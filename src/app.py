7f63f38d37b48c09354dd1c9280a0043880f7db0
Gonzalo Garcia Briceño <gonzalbertogb@gmail.com>
2025-06-02 02:33:51 +0000

    parcial 01-06-2025

--- src/app.py
+++ src/app.py
@@ -1,4 +1,28 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+import numpy as np
+import pickle
 
-# your code here
+# Cargar modelo
+with open('tree_classifier_crit-entro_maxdepth-5_minleaf-4_minsplit2_42.sav', 'rb') as f:
+    model = pickle.load(f)
+
+# Título de la app
+st.title("Predicción Diabetes")
+
+# Inputs del usuario
+n_embarazos = st.number_input('N° de embarazos', min_value=0, step=1)
+nivel_glucosa = st.number_input('Nivel de insulina en sangre', min_value=0.0, step=0.1, format="%.1f")
+presion_sangre = st.number_input('Presion Sanginea', min_value=0.0, step=1.0, format="%.1f")
+nivel_insulina = st.number_input('Insulina en sangre', min_value=0.0, step=1.0, format="%.1f")
+bmi = st.number_input('Indice de masa Corporal', min_value=0.0, step=1.0, format="%.1f")
+dpf = st.number_input('Pedegre de Diabetes', min_value=0.0, step=1.0, format="%.1f")
+edad = st.number_input('Edad', min_value=0.0, step=1)
+
+
+# Botón para predecir
+if st.button('Predecir'):
+    # Preparar datos
+    features = np.array([[n_embarazos, nivel_glucosa, presion_sangre, nivel_insulina, bmi,dpf, edad]])
+    prediction = model.predict(features)
+    labels = ["Negativo", "Positivo"]
+    st.success(f'La flor es: {labels[prediction[0]]}')
